86c194094732c28aa8337a271d49a16e3fc4f980
OranLev <oranlevi11@gmail.com>
2023-05-14 17:07:32 +0300

    Initial Commit

---
+++ CoinBlock/CoinBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinBlockApp.swift
+//  CoinBlock
+//
+//  Created by Oran Levi on 14/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoinBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
