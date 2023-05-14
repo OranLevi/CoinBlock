932b61fe2fa5287140be3de5e894b46523508b8b
OranLev <oranlevi11@gmail.com>
2023-05-14 18:34:14 +0300

    Add Header, Images, ColorSets

---
+++ CoinBlock/Core/Home/Views/HomeView.swift
@@ -0,0 +1,65 @@
+//
+//  HomeView.swift
+//  CoinBlock
+//
+//  Created by Oran Levi on 14/05/2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack{
+            
+            // background
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            // content
+            VStack{
+                homeHeader
+                Spacer(minLength: 0)
+            }
+            
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
+
+extension HomeView{
+    private var homeHeader: some View {
+        HStack{
+            CircleButtonView(iconName: showPortfolio ? "plus"  : "info")
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" :"Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()){
+                        showPortfolio.toggle()
+                    }
+                    
+                }
+        }
+        .padding(.horizontal)
+    }
+}
---
+++ CoinBlock/Extastions/Color.swift
@@ -0,0 +1,26 @@
+//
+//  Color.swift
+//  CoinBlock
+//
+//  Created by Oran Levi on 14/05/2023.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+}
